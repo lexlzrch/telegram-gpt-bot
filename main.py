92a274a2e00e28028b782aaae7ecedd5f8156e09
lexlzrch <alexskullyt2@gmail.com>
2026-04-21 21:04:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 TELEGRAM_TOKEN = os.getenv("8712510143:AAFYpWcZReFqyuoE7IkOr39zyR5UCbj8Shg")
 OPENAI_API_KEY = os.getenv("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")
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI(api_key="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")
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
